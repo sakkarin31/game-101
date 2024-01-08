b94f0b09c54954c228c31c623ec0decab844222f
sakkarin31 <sakkarinlekter@gmail.com>
2024-01-08 16:13:51 +0700

    add backgrounf

---
+++ main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.uix.image import Image
+from kivy.core.window import Window
+from kivy.clock import Clock
+
+class GameWidget(Widget):
+
+    pass
+class Background(Widget):
+    pass
+    
+class MainApp(App):
+    pass
+
+
+if __name__=="__main__":
+    MainApp().run()
