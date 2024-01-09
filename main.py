e76ec7168bd0b4afaea3de14d183f582be3006d8
sakkarin31 <sakkarinlekter@gmail.com>
2024-01-09 23:13:38 +0700

    test to move character

--- main.py
+++ main.py
@@ -4,15 +4,66 @@ from kivy.properties import ObjectProperty
 from kivy.uix.image import Image
 from kivy.core.window import Window
 from kivy.clock import Clock
-from kivy.lang import Builder
+from kivy.graphics import Rectangle
+
 
 class GameWidget(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(
+            self._on_keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_key_down)
+        self._keyboard.bind(on_key_up=self._on_key_up)
+        self.pressed_keys = set()
+        Clock.schedule_interval(self.move_step, 0)
+
+        with self.canvas:
+            self.character = Rectangle(source='character.png', pos=(0, 0), size=(300, 200))
+
+    def _on_keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_key_down)
+        self._keyboard.unbind(on_key_up=self._on_key_up)
+        self._keyboard = None
+
+    def _on_key_down(self, keyboard, keycode, text, modifiers):
+        print('down', text)
+        self.pressed_keys.add(text)
+        if text == 'd':
+            self.character.source = "jumpright.png"
+        if text == 'a':
+            self.character.source = "jumpleft.png"
+    def _on_key_up(self, keyboard, keycode):
+        text = keycode[1]
+        print('up', text)
+        self.character.source = "character.png"
+        if text in self.pressed_keys:
+            self.pressed_keys.remove(text)
+
+    def move_step(self, dt):
+        cur_x = self.character.pos[0]
+        cur_y = self.character.pos[1]
+        step = 200 * dt
+        if 'w' in self.pressed_keys:
+            cur_y += step
+        if 's' in self.pressed_keys:
+            cur_y -= step
+        if 'a' in self.pressed_keys:
+            cur_x -= step
+        if 'd' in self.pressed_keys:
+            cur_x += step
+            
+        self.character.pos = (cur_x, cur_y)
+
+        
+
+
 class Background(Widget):
-    pass
-    
+    pass 
 class MainApp(App):
-    pass
+    def build(self):
+        return GameWidget()
+
+
 
 
 if __name__=="__main__":
