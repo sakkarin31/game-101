a68c6e61d097ec0fc1ae670e3ed723207ac9d2b9
sakkarin31 <sakkarinlekter@gmail.com>
2024-01-10 21:17:33 +0700

    add arrow

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ from kivy.core.window import Window
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, ObjectProperty
+from kivy.graphics import Rectangle
+from random import randint
+
+def collides(rect1, rect2):
+    return (
+        rect1[0] < rect2[0] + rect2[2] and
+        rect1[0] + rect1[2] > rect2[0] and
+        rect1[1] < rect2[1] + rect2[3] and
+        rect1[1] + rect1[3] > rect2[1])
 
 class Background(Widget):
     pass
@@ -12,18 +21,36 @@ class Background(Widget):
 class Character(Image):
     velocity = NumericProperty(0)
 
+class Arrow(Image):
+    velocity = NumericProperty(0)
+
 class GameWidget(Widget):
+    enemy_pos = ObjectProperty((2000, 300))
+    enemy_speed = 600
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.character = Character()
         self.add_widget(self.character)
-        
+
         self._keyboard = Window.request_keyboard(
             self._on_keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_key_down)
         self._keyboard.bind(on_key_up=self._on_key_up)
         self.pressed_keys = set()
         Clock.schedule_interval(self.character_move, 1/60)
+        self.create_enemy()
+
+    def create_enemy(self):
+        self.enemy_pos = (Window.height, randint(1, Window.width + 500))
+        with self.canvas:
+            self.enemy = Rectangle(source='arrowๅ.png', pos=self.enemy_pos, size=(300, 180))
+        self.create_arrow()
+
+    def create_arrow(self):
+        arrow = Arrow(source='arrow.png', pos=self.enemy_pos, size=(50, 50))
+        self.add_widget(arrow)
+        return arrow
 
     def _on_keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_key_down)
@@ -55,12 +82,30 @@ class GameWidget(Widget):
             cur_x -= step
         if 'd' in self.pressed_keys:
             cur_x += step
-            
-        if cur_x < -200 or cur_x > self.width or cur_y < -200 or cur_y > self.height:
+        self.enemy_pos = (self.enemy_pos[1] - self.enemy_speed * dt, self.enemy_pos[0])
+        self.enemy.pos = self.enemy_pos
+
+        if self.enemy_pos[1] < -100:
+            self.create_enemy()
+
+        if collides((cur_x, cur_y, 50, 50), (self.enemy_pos[0], self.enemy_pos[1], self.enemy.size[0] - 200, self.enemy.size[1] - 100)):
             self.stop_game()
-    
+
+        for arrow in self.children:
+            if isinstance(arrow, Arrow) and collides((cur_x, cur_y, 50, 50), (arrow.x, arrow.y, arrow.width, arrow.height)):
+                self.stop_game()
+
+        self.children = [child for child in self.children if not isinstance(child, Arrow) or 0 < child.x < Window.width and 0 < child.y < Window.height]
+
+        for arrow in self.children:
+            if isinstance(arrow, Arrow):
+                arrow.x -= 500 * dt
+
+        if cur_x < -200 or cur_x > Window.width - 200 or cur_y < -200 or cur_y > Window.height - 200:
+            self.stop_game()
+
         self.character.pos = (cur_x, cur_y)
-        
+
     def stop_game(self):
         App.get_running_app().stop()
 
@@ -75,6 +120,3 @@ class MainApp(App):
 
 if __name__ == "__main__":
     MainApp().run()
-    
-    
-    
